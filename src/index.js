d619aa2c758e492112fde0f5ce957210564d6ceb
nirajp82 <61636643+nirajp82@users.noreply.github.com>
2022-10-01 13:22:26 -0400

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,20 @@
+////const helloClass = require('./helloWorld.js');
+import { HelloWorld } from './helloWorld';
+new HelloWorld().sayHello();
+
+
+//const msg = "Shree Raj Shyamaji! Pranam";
+//console.log(msg);
+//console.log(sayPranam);
+
+//function sayPranam() {
+//    console.log(`Pranam!`);
+//}
+
+//function myFunc() { };
+//myFunc.prototype.sayPranam = function () {
+//    console.log(`Pranamji!`);
+//};
+
+//new myFunc().sayPranam();
+
